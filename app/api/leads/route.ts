georgewangyu/ai-radar

feat: add install lead capture

app/api/leads/route.ts
@@ -1,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { radarLeadSchema, saveRadarLead } from "@/lib/radar-leads";
+
+type ErrorResponse = {
+  error?: string;
+  issues?: Record<string, string[] | undefined>;
+};
+
+export async function POST(request: Request) {
+  const payload = await request.json().catch(() => null);
+  const parsed = radarLeadSchema.safeParse(payload);
+
+  if (!parsed.success) {
+    return NextResponse.json(
+      {
+        error: "Invalid lead.",
+        issues: parsed.error.flatten().fieldErrors,
+      } satisfies ErrorResponse,
+      { status: 400 },
+    );
+  }
+
+  if (parsed.data.website) {
+    return NextResponse.json({ ok: true });
+  }
+
+  try {
+    await saveRadarLead({
+      ...parsed.data,
+      product: "ai-radar",
+      repoUrl: "https://github.com/georgewangyu/ai-radar",
+      referrer: request.headers.get("referer") || "",
+      userAgent: request.headers.get("user-agent") || "",
+    });
+
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Could not save lead." },
+      { status: 500 },
+    );
+  }
+}

lib/radar-leads.ts
@@ -1,0 +1,63 @@
+import { z } from "zod";
+
+export const radarLeadSchema = z.object({
+  email: z
+    .string()
+    .trim()
+    .email("Enter a valid email.")
+    .max(254, "Keep the email under 254 characters."),
+  name: z
+    .string()
+    .trim()
+    .min(1, "Enter your name.")
+    .max(120, "Keep the name under 120 characters."),
+  website: z.string().trim().max(0).optional().default(""),
+});
+
+export type RadarLeadInput = z.infer<typeof radarLeadSchema>;
+
+type SaveLeadInput = RadarLeadInput & {
+  product: "ai-radar";
+  repoUrl: string;
+  referrer: string;
+  userAgent: string;
+};
+
+export async function saveRadarLead(input: SaveLeadInput) {
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+  if (!supabaseUrl || !serviceRoleKey) {
+    throw new Error("Missing Supabase lead capture environment configuration.");
+  }
+
+  const response = await fetch(
+    `${supabaseUrl.replace(/\/$/, "")}/rest/v1/radar_leads?on_conflict=product,email`,
+    {
+      method: "POST",
+      headers: {
+        apikey: serviceRoleKey,
+        Authorization: `Bearer ${serviceRoleKey}`,
+        "Content-Type": "application/json",
+        Prefer: "resolution=merge-duplicates",
+      },
+      body: JSON.stringify({
+        email: input.email.toLowerCase(),
+        name: input.name,
+        product: input.product,
+        source: "website-install-gate",
+        consent_updates: true,
+        install_command_revealed: true,
+        repo_url: input.repoUrl,
+        referrer: input.referrer,
+        user_agent: input.userAgent,
+        updated_at: new Date().toISOString(),
+      }),
+    },
+  );
+
+  if (!response.ok) {
+    const body = await response.text();
+    throw new Error(`Supabase lead capture failed: ${response.status} ${body}`);
+  }
+}

georgewangyu/books-radar

app/api/leads/route.ts
@@ -1,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { radarLeadSchema, saveRadarLead } from "@/lib/radar-leads";
+
+type ErrorResponse = {
+  error?: string;
+  issues?: Record<string, string[] | undefined>;
+};
+
+export async function POST(request: Request) {
+  const payload = await request.json().catch(() => null);
+  const parsed = radarLeadSchema.safeParse(payload);
+
+  if (!parsed.success) {
+    return NextResponse.json(
+      {
+        error: "Invalid lead.",
+        issues: parsed.error.flatten().fieldErrors,
+      } satisfies ErrorResponse,
+      { status: 400 },
+    );
+  }
+
+  if (parsed.data.website) {
+    return NextResponse.json({ ok: true });
+  }
+
+  try {
+    await saveRadarLead({
+      ...parsed.data,
+      product: "books-radar",
+      repoUrl: "https://github.com/georgewangyu/books-radar",
+      referrer: request.headers.get("referer") || "",
+      userAgent: request.headers.get("user-agent") || "",
+    });
+
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Could not save lead." },
+      { status: 500 },
+    );
+  }
+}

lib/radar-leads.ts
@@ -1,0 +1,63 @@
+import { z } from "zod";
+
+export const radarLeadSchema = z.object({
+  email: z
+    .string()
+    .trim()
+    .email("Enter a valid email.")
+    .max(254, "Keep the email under 254 characters."),
+  name: z
+    .string()
+    .trim()
+    .min(1, "Enter your name.")
+    .max(120, "Keep the name under 120 characters."),
+  website: z.string().trim().max(0).optional().default(""),
+});
+
+export type RadarLeadInput = z.infer<typeof radarLeadSchema>;
+
+type SaveLeadInput = RadarLeadInput & {
+  product: "books-radar";
+  repoUrl: string;
+  referrer: string;
+  userAgent: string;
+};
+
+export async function saveRadarLead(input: SaveLeadInput) {
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+  if (!supabaseUrl || !serviceRoleKey) {
+    throw new Error("Missing Supabase lead capture environment configuration.");
+  }
+
+  const response = await fetch(
+    `${supabaseUrl.replace(/\/$/, "")}/rest/v1/radar_leads?on_conflict=product,email`,
+    {
+      method: "POST",
+      headers: {
+        apikey: serviceRoleKey,
+        Authorization: `Bearer ${serviceRoleKey}`,
+        "Content-Type": "application/json",
+        Prefer: "resolution=merge-duplicates",
+      },
+      body: JSON.stringify({
+        email: input.email.toLowerCase(),
+        name: input.name,
+        product: input.product,
+        source: "website-install-gate",
+        consent_updates: true,
+        install_command_revealed: true,
+        repo_url: input.repoUrl,
+        referrer: input.referrer,
+        user_agent: input.userAgent,
+        updated_at: new Date().toISOString(),
+      }),
+    },
+  );
+
+  if (!response.ok) {
+    const body = await response.text();
+    throw new Error(`Supabase lead capture failed: ${response.status} ${body}`);
+  }
+}